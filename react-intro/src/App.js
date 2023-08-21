9e281a7d9de852e22e2c87e6564adaef98efc42e
LuizTools <luizfduartejr@gmail.com>
2023-08-21 18:14:13 -0300

    react-intro part 2

--- react-intro/src/App.js
+++ react-intro/src/App.js
@@ -1,12 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './Header';
+import { useState } from 'react';
 
 function App() {
+
+  const [contador, setContador] = useState(0);
+
+  function increment() {
+    setContador(contador + 1);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
+        <Header title="Header Param" />
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello World!</h1>
+        <p>
+          <input type="button" value="Clique" onClick={increment} />
+        </p>
+        <p>{contador}</p> cliques!
       </header>
     </div>
   );
---
+++ react-intro/src/Header.js
@@ -0,0 +1,9 @@
+function Header(props) {
+    return (
+        <header>
+            <h1>{props.title}</h1>
+        </header>
+    );
+}
+
+export default Header;
